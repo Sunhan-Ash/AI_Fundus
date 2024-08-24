86439fcd3c39408f7da3a9e2fa768b571e55997d
Sunhan-Ash <1073128985@qq.com>
2024-08-24 14:21:00 +0800

    add the delete file

---
+++ pytorch-CycleGAN-and-pix2pix-master/pytorch-CycleGAN-and-pix2pix-master/del.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image
+
+# 设置图片文件夹路径
+folder_path = 'your_folder_path_here'
+
+# 遍历文件夹中的所有文件
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+
+    # 检查文件是否为图片
+    try:
+        with Image.open(file_path) as img:
+            width, height = img.size
+            
+            # 检查宽度或高度是否为3215
+            if width == 3215 or height == 3215:
+                print(f"Deleting image: {filename} with size {width}x{height}")
+                os.remove(file_path)
+    except IOError:
+        print(f"File {filename} is not a valid image file.")
