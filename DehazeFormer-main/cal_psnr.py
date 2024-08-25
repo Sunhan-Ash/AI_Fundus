37f3b82b72ee1c9ff7e74e6dc83f14df929c1e06
clowning <1073128985@qq.com>
2024-08-25 17:25:13 +0800

    modify the psnr

--- DehazeFormer-main/cal_psnr.py
+++ DehazeFormer-main/cal_psnr.py
@@ -2,6 +2,8 @@ import os
 import cv2
 import numpy as np
 import pyiqa
+from utils import AverageMeter
+import torch
 
 calculate_psnr = pyiqa.create_metric('psnr', device = 'cuda:0')
 
@@ -15,10 +17,11 @@ def get_image_pairs(dir1, dir2):
 def calculate_psnr_for_directories(dir1, dir2):
     """Calculate PSNR for images in two directories with the same filenames."""
     image_pairs = get_image_pairs(dir1, dir2)
-    psnr_results = {}
+    PSNR = AverageMeter()
+    torch.cuda.empty_cache()
     
     for file1, file2 in image_pairs:
-        # image1 = cv2.imread(file1)
+        image1 = cv2.imread(file1)
         # image2 = cv2.imread(file2)
         
         # if image1 is not None and image2 is not None:
@@ -28,9 +31,9 @@ def calculate_psnr_for_directories(dir1, dir2):
         #     print(f"Could not read images: {file1} or {file2}")
         
         psnr_value = calculate_psnr(file1, file2)
-        psnr_results[os.path.basename(file1)] = psnr_value
+        PSNR.update(psnr_value.item())
         
-    return psnr_results
+    return PSNR.avg
 
 # Example usage
 dir1 = './data/fake_temp/train/hazy'
