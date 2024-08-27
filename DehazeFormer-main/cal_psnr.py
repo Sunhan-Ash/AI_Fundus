f21d128198bc5d622e08c34fb940ba8f0edcc58f
clowning <1073128985@qq.com>
2024-08-27 14:52:18 +0800

    add piqe

--- DehazeFormer-main/cal_psnr.py
+++ DehazeFormer-main/cal_psnr.py
@@ -6,7 +6,8 @@ from utils import AverageMeter
 import torch
 
 calculate_psnr = pyiqa.create_metric('psnr', device = 'cuda:0')
-calculate_MUSIQ = pyiqa.create_metric('musiq', device = 'cuda:0')
+calculate_MUSIQ = pyiqa.create_metric('piqe', device = 'cuda:0')
+# calculate_FID = pyiqa.create_metric('fid', device = 'cuda:0')
 
 def get_image_pairs(dir1, dir2):
     """Get list of common image file paths in two directories."""
