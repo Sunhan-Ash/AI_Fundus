c111b02f40d8d25d26b6d2104749453c9a8266f4
Sunhan-Ash <1073128985@qq.com>
2024-09-03 14:40:22 +0800

    s

--- dataset_get_mask.py
+++ dataset_get_mask.py
@@ -22,14 +22,11 @@ for source_dir in source_dirs:
         image = cv2.imread(image_path)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
-        # 创建一个与灰度图像大小相同的空白掩码
-        mask = np.zeros_like(gray)
+        # 创建一个与灰度图像大小相同的掩码，并将所有非零像素设置为1（感兴趣区域）
+        mask = np.ones_like(gray)
         
-        # 将灰度图像中所有非零的区域设置为1（表示前景），零的区域保持为0（表示背景/边界）
-        mask[gray > 0] = 1
-
-        # 反转掩码，边界部分为1，其他部分为0
-        mask = 1 - mask
+        # 将灰度图像中值为0的区域设置为0（表示边界）
+        mask[gray == 0] = 0
         
         # 将掩码转换为二值图像格式（0和255）
         binary_mask = mask * 255
@@ -39,5 +36,6 @@ for source_dir in source_dirs:
         os.makedirs(mask_dir, exist_ok=True)
         save_path = os.path.join(mask_dir, os.path.basename(image_path).replace('.jpeg', '.jpeg'))
         cv2.imwrite(save_path, binary_mask)  # 保存为二值图像格式
+
 
 print('Done!')
